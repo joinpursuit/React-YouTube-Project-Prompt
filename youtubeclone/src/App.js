4aa737c91cfb07f76b68df2e978d8d872f5d0ac8
Cari <t.carina@rocketmail.com>
2021-05-18 20:58:27 -0400

    initial commit

---
+++ youtubeclone/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/navbar'
+import About from './components/aboutPage'
+import { Route } from 'react-router-dom'
+import { Switch } from 'react-router-dom'
+
+function App() {
+  return (
+    <div className="App">
+       <Navbar />
+       <Switch>
+         {/* <Route exact path='/' component={ Home } /> */}
+         <Route path='/about' component={ About } />
+       </Switch>
+    </div>
+  );
+}
+
+export default App;
---
+++ youtubeclone/src/components/aboutPage.js
@@ -0,0 +1,35 @@
+import React from 'react'
+
+
+const About = () => {
+    
+    return(
+        <main>
+            <h1>About Project & Developers</h1>
+
+            <p>This app is created using React, Javascript, the YouTube API, and CSS. <br />It is a user-friendly mock-up of the popular video streaming platform.<br />This application was created from the ground up by a team of diverse women.</p>
+
+            <h1>AnJu</h1>
+            <p> 
+            I am a performing artist and software engineer who works at the intersection of theatre arts, technology, and social equity.<br />  I am passionate about accessibility, advocacy, building community, and financial literacy. I desire to design products that help communities thrive, especially during times of need. <br />My goal is to join a company that creates products or offers services that cater to the local or larger community.
+            </p>
+
+
+            <h1>Carina</h1>
+            <p>
+            My name is Carina and I was born and raised in Queens. I am passionate about music, media, mental health and helping those who have a lack of representation.<br /> I want to use my skills to give back to my community and perhaps one day also help others better thier lives. <br />I hope to be able to use my love of tech to be able to connect my different passions.
+
+            </p>
+
+            <h1>Jessica</h1>
+            <p>
+
+            </p>
+
+
+
+        </main>
+    )
+}
+
+export default About
