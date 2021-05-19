d36481555d2c4681fc13fb02616dfdc382a4e92c
Rayvon Finney Pinkston <rayfinn55@gmail.com>
2021-05-19 15:45:52 -0400

    some work

--- you_tube.project/src/App.js
+++ you_tube.project/src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar'
+import { Switch, Route } from 'react-router-dom'
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar/>
+         <main>
+           <Switch>
+             <Route exact path ="/"><h1>Youtube: Pick a video!</h1></Route>
+           </Switch>
+                
+         </main>
+                
     </div>
   );
 }
