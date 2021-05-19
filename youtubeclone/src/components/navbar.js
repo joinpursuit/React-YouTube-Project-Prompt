54877533ee70675eb5944d2370d532ccd2f05366
anjuhypp <anjuhypp@gmail.com>
2021-05-18 22:01:53 -0400

    add navbar css and image

--- youtubeclone/src/components/navbar.js
+++ youtubeclone/src/components/navbar.js
@@ -1,8 +1,11 @@
 import { NavLink } from 'react-router-dom'
+import './navbar.css'
+import { Link } from "react-router-dom"
 
 const Navbar = () => {
     return (
-        <nav>
+        <nav className="NavBarContainer">
+            <Link><img src="https://cdn130.picsart.com/301451482048211.png?r1024x1024" alt="YouTube logo"/></Link>
             <NavLink to='/'>Home</NavLink>
             <NavLink to ='/about'>About</NavLink> 
         </nav>
