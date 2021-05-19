a0cf849242ce5f3b32346517680c502cbeb48b55
Raymond Udeogu <rfizzle25@gmail.com>
2021-05-19 14:11:59 -0400

    initial commit

---
+++ fsw-react-youtube/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import NavBar from './Components/NavBar';
+
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      <h1>Youtube App</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ fsw-react-youtube/src/Components/NavBar.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Route, Link } from "react-router-dom"
+
+const NavBar = () => {
+    return(
+        <div>
+            <nav>
+                <h1>Show</h1>
+            </nav>
+        </div>
+    )
+}
+
+export default NavBar;
