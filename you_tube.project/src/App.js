1370a4c6dc296259f51ba1b8a5a5683b0f466bb5
Alexander <alexanderl96@yahoo.com>
2021-05-19 14:15:54 -0400

    dsfd

--- you_tube.project/src/App.js
+++ you_tube.project/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import Channel from "./Channel"
+import {Route, Link} from 'react-router-dom'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src='http://cdn.onlinewebfonts.com/svg/img_24879.png' className="App-logo" alt="logo" />
+        <h1>
+          Welcome to my Channel.
+        </h1>
+        <Link to="/">VISIT</Link>
+        
       </header>
+      <Route path='/' component={Channel}/>
     </div>
   );
 }
--- you_tube.project/src/index.js
+++ you_tube.project/src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {BrowersRouter} from 'react-router-dom'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  
+    <BrowersRouter>
+      <App />
+    </BrowersRouter>,
+
   document.getElementById('root')
 );
 
