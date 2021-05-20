7b19474e82a0ac790546c7ffaf8216ce00a2320e
Raymond Udeogu <rfizzle25@gmail.com>
2021-05-20 11:18:39 -0400

    initial commit

--- fsw-react-youtube/src/Components/NavBar.js
+++ fsw-react-youtube/src/Components/NavBar.js
@@ -1,11 +1,11 @@
 import React from "react";
-import { Route, Link } from "react-router-dom"
+import { Link } from "react-router-dom"
 
 const NavBar = () => {
     return(
         <div>
             <nav>
-                <h1>Show</h1>
+                
             </nav>
         </div>
     )
