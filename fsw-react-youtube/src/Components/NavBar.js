bf4e06d0a1f53749e7163cef27c6ae5279029541
Raymond Udeogu <rfizzle25@gmail.com>
2021-05-20 22:42:07 -0400

    day work

--- fsw-react-youtube/src/Components/NavBar.js
+++ fsw-react-youtube/src/Components/NavBar.js
@@ -5,7 +5,9 @@ const NavBar = () => {
     return(
         <div>
             <nav>
-                
+                <Link to="/">Home</Link>{" "}
+                <Link to="/about">About</Link>{" "}
+
             </nav>
         </div>
     )
