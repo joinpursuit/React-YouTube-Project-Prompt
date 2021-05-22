28f82e81a87f440da7c9d13a600e2b85032f3253
Carina <43793452+tCarina@users.noreply.github.com>
2021-05-22 09:49:48 -0400

    homepage (#5)
    Co-authored-by: jessicanetto <75263227+jessicanetto@users.noreply.github.com>

--- youtubeclone/src/App.js
+++ youtubeclone/src/App.js
@@ -1,17 +1,17 @@
-import './App.css';
-import Navbar from './components/navbar'
-import About from './components/aboutPage'
-import { Route } from 'react-router-dom'
-import { Switch } from 'react-router-dom'
-
+import "./App.css";
+import Navbar from "./components/navbar";
+import About from "./components/aboutPage";
+import { Route } from "react-router-dom";
+import { Switch } from "react-router-dom";
+import home from "./components/home";
 function App() {
   return (
     <div className="App">
-       <Navbar />
-       <Switch>
-         {/* <Route exact path='/' component={ Home } /> */}
-         <Route path='/about' component={ About } />
-       </Switch>
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={home} />
+        <Route path="/about" component={About} />
+      </Switch>
     </div>
   );
 }
