f9d60cb968090b65d41a9ec9f71960f46e276be8
Cari <t.carina@rocketmail.com>
2021-05-22 12:58:56 -0400

    create youtube component

---
+++ youtubeclone/src/components/youtube.js
@@ -0,0 +1,18 @@
+import {useState, useEffect } from 'react'
+
+const Youtube = () => {
+
+    const [vidList, setvidList] = useState([]) 
+
+    
+
+    return (
+        <section>
+            <h1>Hello World</h1>
+        </section>
+    )
+
+
+}
+
+export default Youtube
