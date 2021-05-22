cf85ec4e2b652adab6a9904dd67d795f9da6be1c
Cari <t.carina@rocketmail.com>
2021-05-22 10:58:41 -0400

    add github links

--- youtubeclone/src/components/aboutPage.js
+++ youtubeclone/src/components/aboutPage.js
@@ -1,37 +1,51 @@
-import React from 'react'
-
+import React from "react";
 
 const About = () => {
-    
-    return(
-        <main>
-            <h1>About Project & Developers</h1>
+  return (
+    <main>
+      <h1>About Project & Developers</h1>
 
-            <p>This app is created using React, Javascript, the YouTube API, and CSS. <br />It is a user-friendly mock-up of the popular video streaming platform.<br />This application was created from the ground up by a team of diverse women.</p>
+      <p>
+        This app is created using React, Javascript, the YouTube API, and CSS.{" "}
+        <br />
+        It is a user-friendly mock-up of the popular video streaming platform.
+        <br />
+        This application was created from the ground up by a team of diverse
+        women.
+      </p>
+      <br />
 
-            <h1>AnJu</h1>
-            <p> 
-            I am a performing artist and software engineer who works at the intersection of theatre arts, technology, and social equity.<br />  I am passionate about accessibility, advocacy, building community, and financial literacy. I desire to design products that help communities thrive, especially during times of need. <br />My goal is to join a company that creates products or offers services that cater to the local or larger community.
-            </p>
+      <h1>AnJu</h1>
+      <p>
+        I am a performing artist and software engineer who works at the
+        intersection of theatre arts, technology, and social equity.
+        <br /> I am passionate about accessibility, advocacy, building
+        community, and financial literacy. I desire to design products that help
+        communities thrive, especially during times of need. <br />
+        My goal is to join a company that creates products or offers services
+        that cater to the local or larger community.
+      </p>
+      <a href='https://github.com/AnJuHyppolite' target='_blank' rel="noreferrer noopener">My Github</a>
+      <h1>Carina</h1>
+      <p>
+        My name is Carina and I was born and raised in Queens. I am passionate
+        about music, media, mental health and helping those who have a lack of
+        representation.
+        <br /> I want to use my skills to give back to my community and perhaps
+        one day also help others better thier lives. <br />I hope to be able to
+        use my love of tech to be able to connect my different passions.
+      </p>
+      <a href='https://github.com/tCarina' target='_blank' rel="noreferrer noopener">My Github</a>
+      <h1>Jessica</h1>
+      <p>
+        I am a software engineer and biomedical researcher with a background in
+        neurotechnology. I aspire to apply my interest in technologies like AI
+        and VR to improve patient experience and increase accessibility in
+        healthcare.
+      </p>
+      <a href='https://github.com/jessicanetto' target='_blank' rel="noreferrer noopener">My Github</a>
+    </main>
+  );
+};
 
-
-            <h1>Carina</h1>
-            <p>
-            My name is Carina and I was born and raised in Queens. I am passionate about music, media, mental health and helping those who have a lack of representation.<br /> I want to use my skills to give back to my community and perhaps one day also help others better thier lives. <br />I hope to be able to use my love of tech to be able to connect my different passions.
-
-            </p>
-
-            <h1>Jessica</h1>
-            <p>
-
-            I am a software engineer and biomedical researcher with a background in neurotechnology. I aspire to apply my interest in technologies like AI and VR to improve patient experience and increase accessibility in healthcare.
-
-            </p>
-
-
-
-        </main>
-    )
-}
-
-export default About
+export default About;
