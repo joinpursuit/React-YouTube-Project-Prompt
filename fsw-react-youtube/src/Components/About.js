0abe607b73104101e76a0bee59a81cc22587eff2
Jon Narine <jonnicwolf@gmail.com>
2021-05-26 16:41:19 -0400

    About bios

--- fsw-react-youtube/src/Components/About.js
+++ fsw-react-youtube/src/Components/About.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from "react";
 
 export default function About() {
-    return (
-        <div>
-            <h1>hello</h1>
-        </div>
-    )
+  return (
+    <>
+      <div className="About">
+        <h2>About Page</h2>
+      </div>
+      <div>
+        <h4>
+          Project Description: Create a Youtube App in React using the Youtube
+          API, then choose a video from a list to watch.
+        </h4>
+        <h4>Team</h4>
+        <p>
+          Raymond Udeogu. Software Engineer with a passion for athletics and
+          cinema.
+        </p>
+        <a href="https://github.com/rayu117">Ray's Github </a>
+              <br />
+              Jon Narine | Fezz. Software Engineer with a passion for Motorsport, Bar Culture and Jazz
+              <p>Jon Narine.</p>
+              <a href="https://github.com/jonnicwolf">
+                  Jon's Github
+              </a>
+      </div>
+    </>
+  );
 }
