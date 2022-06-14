5f1b69ccf1d633c2619cc44080b0caf8034a53ee
Denisse Maldonado <denissemaldonado@Denisses-MacBook-Air.local>
2022-06-13 21:10:46 -0400

    adds .env

--- youtube-app/src/App.js
+++ youtube-app/src/App.js
@@ -8,6 +8,34 @@ export default function App() {
   if (isLoading) return "Loading..." ;
   if (error) return "Error"
 
+
+  const useFetch = (url, options) => {
+    const [response, setResponse] = React.useState(null);
+    const [error, setError] = React.useState(null);
+    const [abort, setAbort] = React.useState(() => {});
+  
+    React.useEffect(() => {
+      const fetchData = async () => {
+        try {
+          const abortController = new AbortController();
+          const signal = abortController.signal;
+          setAbort(abortController.abort);
+          const res = await fetch(url, {...options, signal});
+          const json = await res.json();
+          setResponse(json);
+        } catch (error) {
+          setError(error);
+        }
+      };
+      fetchData();
+      return () => {
+        abort();
+      }
+    }, []);
+  
+    return { response, error, abort };
+  };
+
   return (
     <div className="App">
       <header className="App-header">
